a93705f33d4c530fe05b1f7fb5c28194e2a420fb
Alexander Flanagan <a.flanagan@protera.com>
2024-12-23 10:42:57 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/atthehorizon/ai-dm-api
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, players! I will be your new Dungeon Master.")
+}
